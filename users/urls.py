a2d4682f9081c9a19d35b8ec75f38dac5aceea76
bhuva_prashant_085 <152417485+BhuvaPrashant085@users.noreply.github.com>
2026-08-08 23:17:21 +0530

    Rename login route from 'login1' to 'login'

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
 
     
-    path('login/', views.login_user, name='login1'),
+    path('login/', views.login_user, name='login'),
      path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_user, name='logout'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
